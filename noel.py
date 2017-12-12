735fac77520f8c922f535d2e92b7131e5e50b0da
Nicolas Denoyelle <denoyelle.nicolas@gmail.com>
2017-12-12 12:20:00 +0100

    update names and emails and links to avoid

--- noel.py
+++ noel.py
@@ -4,17 +4,29 @@ import smtplib
 
 #Les personnes qui s'offrent des cadeaux et leur adresse email
 personnes = {
-    'personne1':{'email':''},
-    'personne2':{'email':''}
+    'personne1':{
+        'email':'personne1@gmail.com', #email de la personne
+        'eviter':[] #On n'offre pas de cadeau aux personnes dans cette liste
+    },
+    'personne2':{
+        'email':'personne2@gmail.com',
+        'eviter':['personne3']
+    },
+    'personne3':{
+        'email':'personne3@gmail.com',
+        'eviter':['personne2']
+    },
+    'personne4':{
+        'email':'personne4@gmail.com',
+        'eviter':['personne5']
+    },
+    'personne5':{
+        'email':'personne5@gmail.com',
+        'eviter':['personne4']
+    }    
 }
 
-#Ne pas offrir de cadeaux entre ces liens
-liens_a_eviter = {
-    'personne1': [],
-    'personne2' : []
-}
-
-#Themes des cadeau à renseigner par personne
+#Themes des cadeau à renseigner. On offre et reçoit autant de cadeau que d'élément dans cette liste.
 types  = [('taille1','theme1'),('taille2','theme2')]
 
 #Configuration pour l'envoi des mails.
@@ -27,35 +39,35 @@ mot_de_passe_smtp='' #'motdepass'e
 ############################ Definition des fonctions utiles au tirage au sort  #########################################
 #########################################################################################################################
     
-def build_graph(peoples, types, rm_links):
+def build_graph(peoples, types):
     g = ig.Graph(directed=True)
     g.add_vertices(list(peoples.keys()))
                    
     for src in g.vs:
-        targets = [ tgt for tgt in g.vs if tgt!=src and tgt['name'] not in rm_links[src['name']] ]
+        targets = [ tgt for tgt in g.vs if tgt!=src and tgt['name'] not in peoples[src['name']]['eviter'] ]
         for dst in targets:
             for t in types:
-                g.add_edge(src, dst, type=t)        
+                g.add_edge(src, dst, type_=t)        
     return g
 
 def plot(g):
-    ig.plot(g, vertex_label = g.vs['name'], edge_label=[ e['type'][0] for e in g.es ])
+    ig.plot(g, vertex_label = g.vs['name'], edge_label=[ e['type_'][0] for e in g.es ])
 
 def remove_path(g, path):
     #plot(g)
     for e0 in path.es:
         source   = path.vs[e0.source]['name']
         target   = path.vs[e0.target]['name']
-        type     = e0['type']
+        type_     = e0['type_']
         rm_edges = []
         for e in g.es:            
             src = g.vs[e.source]['name']
             tgt = g.vs[e.target]['name']
-            tp  = e['type']
+            tp  = e['type_']
 
             if ((src == source and tgt == target)  or
-                (src == source and type == tp)  or
-                (tgt == target and type == tp)):
+                (src == source and type_ == tp)  or
+                (tgt == target and type_ == tp)):
                 rm_edges.append(e)
         g.delete_edges(rm_edges)
         #plot(g)
@@ -81,7 +93,7 @@ def random_circuit(g):
             edge = g.es[random.randint(0, len(g.es)-1)]
             source= edge.source
             target = edge.target
-            init_edges = [ (e.source, e.target, e['type'], g.vs[e.source]['name'], g.vs[e.target]['name']) for e in g.es if e.target == source ]
+            init_edges = [ (e.source, e.target, e['type_'], g.vs[e.source]['name'], g.vs[e.target]['name']) for e in g.es if e.target == source ]
             g.delete_edges([ e for e in g.es if e.index != edge.index and (e.target == source or e.target == target or e.source == source) ])
             return target, source, init_edges
         
@@ -108,7 +120,7 @@ def random_circuit(g):
     if len(init_edges) == 0 or len(tmp.es) < len(tmp.vs)-1:
         return None
     init_edge = init_edges[random.randint(0, len(init_edges)-1)]
-    tmp.add_edge(init_edge[3], init_edge[4], type=init_edge[2])
+    tmp.add_edge(init_edge[3], init_edge[4], type_=init_edge[2])
     #plot(tmp)
     return tmp
 
@@ -118,8 +130,8 @@ def print_tirage(tirage):
     for e in tirage.es:
         donneur = tirage.vs[e.source]['name']
         receveur= tirage.vs[e.target]['name']
-        taille  = e['type'][0]
-        theme= e['type'][1]
+        taille  = e['type_'][0]
+        theme= e['type_'][1]
         print( "\t%10s offre un %10s cadeau à %10s sur le thème %10s" % (donneur, taille, receveur, theme) )
 
 def envoyer_email(tirage,
@@ -139,8 +151,8 @@ def envoyer_email(tirage,
     for e in tirage.es:
         donneur = tirage.vs[e.source]['name']
         receveur= tirage.vs[e.target]['name']
-        taille  = e['type'][0]
-        theme= e['type'][1]
+        taille  = e['type_'][0]
+        theme= e['type_'][1]
         toaddr   = personnes[donneur]['email']
         fromaddr = 'pere@noel.fr'
         msg = ("From: %s\r\nTo: %s\r\n\r\n"
@@ -158,7 +170,7 @@ def envoyer_email(tirage,
 
 def faire_les_tirages():
     tirages = []
-    g = build_graph(personnes, types, liens_a_eviter)
+    g = build_graph(personnes, types)
 
     for i in range(len(types)):
         #plot(g)
@@ -180,7 +192,6 @@ def verifier_tirages(tirages):
     ret = False
     participants = personnes.copy()
     for p in participants.keys():
-        participants[p] = {}
         participants[p]['type_to']   = []
         participants[p]['type_from'] = []
         participants[p]['receveur']  = []
@@ -194,20 +205,20 @@ def verifier_tirages(tirages):
         for e in tirage.es:
             donneur = tirage.vs[e.source]['name']
             receveur= tirage.vs[e.target]['name']
-            type  = e['type']
-            participants[donneur]['type_to'].append(type)
+            type_  = e['type_']
+            participants[donneur]['type_to'].append(type_)
             participants[donneur]['receveur'].append(receveur)
-            participants[receveur]['type_from'].append(type)
-    
-    for p in participants.keys():
-        for r in participants[p]['receveur']:
-            if r in liens_a_eviter[p]:
+            participants[receveur]['type_from'].append(type_)
+
+    for p in participants.items():
+        for r in p[1]['receveur']:
+            if r in p[1]['eviter']:
                 print('%s ne doit pas offrir à %s.' % (p,r))
                 return True
 
-        for k in participants[p].keys():
-            if has_duplicate(participants[p][k]):
-                print("Duplicate in %s %s %s" % (p, k, str(participants[p][k])))
+        for k in p[1].keys():
+            if type(p[1][k]) == list and has_duplicate(p[1][k]):
+                print("Duplicate in %s %s %s" % (p[0], k, str(p[1][k])))
                 return True
     return False
 
